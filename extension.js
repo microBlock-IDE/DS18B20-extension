2341c8d567e93816fd90ece567178847658bb7c9
Max <7087907+maxpromer@users.noreply.github.com>
2024-02-12 04:03:14 +0700

    Update extension.js

--- extension.js
+++ extension.js
@@ -7,17 +7,17 @@
     icon: "/static/icon.png", // Category icon
     color: "#2C3E50", // Category color (recommend some blocks color)
     blocks: [ // Blocks in Category
-    {
-        xml: `
-            <block type="ds18b20_read">
-                <value name="pin">
-                    <shadow type="math_number">
-                        <field name="NUM">2</field>
-                    </shadow>
-                </value>
-            </block>
-        `
-    },
+        {
+            xml: `
+                <block type="ds18b20_read">
+                    <value name="pin">
+                        <shadow type="math_number">
+                            <field name="NUM">2</field>
+                        </shadow>
+                    </value>
+                </block>
+            `
+        },
     ],
     supportArduinoPlatform: true,
     depends: [ // Arduino library
